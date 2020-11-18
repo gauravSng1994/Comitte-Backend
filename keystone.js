2ee020fe06507530e983ad446e4988b7a20afa05
rahul <rahul@airwhizz.com>
2020-11-18 17:20:51 +0530

    set in process env

--- keystone.js
+++ keystone.js
@@ -1,10 +1,27 @@
 var keystone = require('keystone');
 
 keystone.init({
+  'name': 'Hospital app',
   'cookie secret': 'secure string goes here',
   'user model': 'User',
   'auto update': true,
   'auth': true,
+  'favicon': 'public/favicon.ico',
+  'less': 'public',
+  'static': 'public',
+  'views': 'templates/views',
+  'view engine': 'pug',
+  'mongo': process.env.MONGO_URI || process.env.MONGOLAB_URI || 'mongodb://localhost/hospital',
 });
 keystone.import('models');
+keystone.set('locals', {
+	_: require('lodash'),
+	env: keystone.get('env'),
+	utils: keystone.utils,
+	editable: keystone.content.editable,
+	ga_property: keystone.get('ga property'),
+	ga_domain: keystone.get('ga domain'),
+	chartbeat_property: keystone.get('chartbeat property'),
+	chartbeat_domain: keystone.get('chartbeat domain')
+});
 keystone.start();
--- models/UserRole.js
+++ models/UserRole.js
@@ -1,16 +1,17 @@
 var keystone = require('keystone');
+var Types = keystone.Field.Types;
 
 var UserRole = new keystone.List('UserRole');
 
 UserRole.add({
-  displayName: {type: String },
-  email: { type: keystone.Field.Types.Email, unique: true },
-  password: { type: keystone.Field.Types.Password },
+  name: { type: String, required: true, initial: true },
+ 
 });
 
 UserRole.schema.virtual('canAccessKeystone').get(function () {
   return true;
 });
 
-UserRole.defaultColumns = 'id, displayName, email';
+
+UserRole.defaultColumns = 'name';
 UserRole.register();
